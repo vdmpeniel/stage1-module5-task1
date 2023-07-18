13152c7335f8a467b6bc0bf360ad30f7b9b6ddf1
victor diaz <vdmpeniel@yahoo.es>
2023-07-18 08:43:38 -0400

    Added all required changes

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,32 +1,43 @@
 package com.epam.mjc;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            return list.stream().allMatch(str -> Character.isUpperCase(str.charAt(0)));
+        };
+
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            list.addAll( list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList()) );
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return () -> values.stream().filter(v -> Character.isUpperCase(v.charAt(0)) && v.endsWith(".") && v.split(" ").length > 3  ).collect(Collectors.toList());
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            return list.stream().collect(Collectors.toMap(value -> value, String::length));
+        };
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (listA, listB) -> {
+            List<Integer> listC = new ArrayList<>(listA);
+            listC.addAll(listB);
+            return listC;
+        };
     }
 }
